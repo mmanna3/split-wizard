4e23d0445bc83b31a862402fc041f8f72cf9d430
mmmannna <mmmannna3@gmail.com>
2022-04-23 20:17:38 -0300

    Toquetea y arregla reglas de eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import ListaIntegrantes from './ListaIntegrantes';
-import './App.css';
+import ListaIntegrantes from "./ListaIntegrantes";
+import "./App.css";
 
 function App() {
   const estilos = {
     contenedor: {
-      height: '100%',
-      position: 'absolute', // esto debería andar
-      left: '0px',
-      width: '100%',
-      overflow: 'hidden',
-      paddingLeft: '1em',
-      background: 'linear-gradient(90deg, rgba(77,1,94,1) 0%, rgba(121,9,102,1) 21%, rgba(234,0,255,1) 70%)',
+      height: "100%",
+      // position: "absolute", // esto debería andar
+      left: "0px",
+      width: "100%",
+      overflow: "hidden",
+      paddingLeft: "1em",
+      // background:
+      // "linear-gradient(90deg, rgba(77,1,94,1) 0%, rgba(121,9,102,1) 21%, rgba(234,0,255,1) 70%)",
     },
     titulo: {
-      color: '#eee',
-      marginLeft: '1em',
+      color: "#eee",
+      marginLeft: "1em",
     },
   };
 
--- src/ListaIntegrantes.tsx
+++ src/ListaIntegrantes.tsx
@@ -1,30 +1,30 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function ListaIntegrantes() {
   const estilos = {
     contenedor: {
-      margin: '1em',
-      border: 'solid 1px #333',
-      borderRadius: '5px',
-      padding: '2em',
+      margin: "1em",
+      border: "solid 1px #333",
+      borderRadius: "5px",
+      padding: "2em",
     },
     input: {
-      padding: '10px',
-      border: 'solid 1px #555',
+      padding: "10px",
+      border: "solid 1px #555",
     },
     boton: {
-      border: 'solid 1px #555',
-      borderRadius: '5px',
-      padding: '10px',
-      marginLeft: '10px',
+      border: "solid 1px #555",
+      borderRadius: "5px",
+      padding: "10px",
+      marginLeft: "10px",
       // color: '#eee',
-      backgroundColor: '#eda726',
+      backgroundColor: "#eda726",
     },
   };
 
   const [lista, setLista] = useState<string[]>([]);
-  const [nuevoIntegrante, setNuevoIntegrante] = useState('');
+  const [nuevoIntegrante, setNuevoIntegrante] = useState("");
 
   const agregarIntegrante = () => {
     setLista((listaAnterior) => [...listaAnterior, nuevoIntegrante]);
@@ -33,12 +33,26 @@ function ListaIntegrantes() {
   return (
     <div style={estilos.contenedor}>
       <div>
-        {lista.map((integrante) => <div>{integrante}</div>)}
+        {lista.map((integrante) => (
+          <div>{integrante}</div>
+        ))}
       </div>
       <div>
-        <div style={{ marginTop: '1em', marginBottom: '1em' }}>
-          <input style={estilos.input} type="text" value={nuevoIntegrante} onChange={(event) => setNuevoIntegrante(event.target.value)} placeholder="Nombre. Ej: Gandalf" />
-          <button type="button" style={estilos.boton} onClick={() => agregarIntegrante()}>Agregar</button>
+        <div style={{ marginTop: "1em", marginBottom: "1em" }}>
+          <input
+            style={estilos.input}
+            type="text"
+            value={nuevoIntegrante}
+            onChange={(event) => setNuevoIntegrante(event.target.value)}
+            placeholder="Nombre. Ej: Gandalf"
+          />
+          <button
+            type="button"
+            style={estilos.boton}
+            onClick={() => agregarIntegrante()}
+          >
+            Agregar
+          </button>
         </div>
       </div>
     </div>
