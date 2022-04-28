ed3e808d27c28b399ed55e00786257a38dfaad42
Martin Ferrarese <ferra@Martins-MacBook-Pro.local>
2022-04-28 12:39:29 -0300

    Push bobo para probar gitpages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('test de ejemplo de react', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
